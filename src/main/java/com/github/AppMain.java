464a8e03b7be3dc87f105d3f58ad19926be20dad
richard1230 <yongjinhu1230@gmail.com>
2020-08-25 11:03:59 +0800

    NoSuchMethodError

---
+++ src/main/java/com/github/AppMain.java
@@ -0,0 +1,9 @@
+package com.github;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+
+public class AppMain {
+    public static void main(String[] args) {
+        BeanUtils.init();
+    }
+}
---
+++ src/main/java/com/github/HelloWorldService.java
@@ -0,0 +1,14 @@
+package com.github;
+
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestSchema(schemaId = "hello")
+@RequestMapping("/")
+public class HelloWorldService {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+}
